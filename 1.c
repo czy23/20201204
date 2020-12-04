445a9e01796987ed5255db455d28edbe7419163f
czy23 <632022875@qq.com>
2020-12-04 01:01:07 -0500

    two dog

--- 1.c
+++ 1.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
 int main(int argc, const char *argv[])
 {
+	int num;
 	printf("hello linux\n");
 	printf("hello develop\n");
-        printf("this is czy\n");
-	返回 0 ;
+    printf("this is czy\n");
+		
+
+		return 0 ;
 }
